0db166bd6d2d7ebabd30d739c6295b5eeeb193d7
Mansour266 <mansour24@protonmail.com>
2022-05-10 18:02:29 +0200

    Solution accepted with 30ms and memory 12kb

---
+++ Codeforces/112A - A. Petya and Strings.cpp
@@ -0,0 +1,25 @@
+/*
+ * This is solution to problem 112A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/112/A
+ * Author: Mansour
+ * Last modified : 9/5/2022
+ * */
+
+#include <iostream>
+#include <algorithm>// For converting the strings to lowercase
+int solution_a_112(){
+    std::string first_letters, second_letters;
+    std::cin >> first_letters >> second_letters;
+    //Now convert both strings to be lowercase both
+    std::transform(first_letters.begin(), first_letters.end(), first_letters.begin(), ::tolower);
+    std::transform(second_letters.begin(), second_letters.end(), second_letters.begin(), ::tolower);
+    //Let's begin test cases
+    if(first_letters < second_letters)
+        std::cout << "-1";
+
+    else if(first_letters > second_letters)
+        std::cout << "1";
+
+    else if(first_letters == second_letters)
+        std::cout << "0";
+}
