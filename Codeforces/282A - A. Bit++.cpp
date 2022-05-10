240baa85a706f658be5e90bd363bc4df07b76a9f
Mansour266 <mansour24@protonmail.com>
2022-05-10 17:24:16 +0200

    Solution accepted with 15ms and memory 0kb

---
+++ Codeforces/282A - A. Bit++.cpp
@@ -0,0 +1,24 @@
+/*
+ * This is solution to problem 282A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/282/A
+ * Author: Mansour
+ * Last modified : 9/5/2022
+ * */
+
+#include <iostream>
+
+int main(){
+    int n,x{0};// N is the number of operations and X is the variable that operations are done on
+    std::string operation;//This should handle whether the operation is ++ or --
+    std::cin >> n;
+    if(n >= 1 && n <= 150) {//Check the constraints
+        for (int i{0}; i < n; ++i) {
+        std::cin >> operation;
+        if(operation[1] == '+')
+            x++;
+        else if(operation[1] == '-')
+            x--;
+        }
+        std::cout << x;
+    }
+}
