4b96cf4836c38b2256d6f14b4bb8734d78d3666e
Mansour266 <mansour24@protonmail.com>
2022-05-12 21:07:54 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/236A - A. Boy or Girl .cpp
@@ -0,0 +1,24 @@
+/*
+ * This is solution to problem 236A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/236/A
+ * Author: Mansour
+ * Last modified : 12/5/2022
+ * */
+
+#include <iostream>
+#include <algorithm>// To sort the letters
+
+void solution_a_236(){
+    std::string word;
+    std::cin >> word;
+
+    sort(word.begin(), word.end());
+    /*
+     * Now keep the unique the letters and remove any duplications*/
+    word.erase(std::unique(word.begin(), word.end()),word.end());
+
+    if(word.length() % 2 == 0)
+        std::cout << "CHAT WITH HER!";
+    else
+        std::cout << "IGNORE HIM!";
+}
