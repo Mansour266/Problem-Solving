ce75964cf468ff95c230f3d1b76287dbf6091267
Mansour266 <mansour24@protonmail.com>
2022-05-24 20:24:41 +0200

    Solution accepted with 77ms and memory 100kb

--- Codeforces/C1676 - B. Equal Candies .cpp
+++ Codeforces/C1676 - B. Equal Candies .cpp
@@ -14,29 +14,32 @@ int main(){
     int test_cases;
 
     std::cin >> test_cases;
+    if(test_cases >= 1 && test_cases <= 1000) {//Only to satisfy constraints
 
-    //This should loop until all test cases are done
-    for(int i {0}; i < test_cases; ++i){
-    int boxes_no;
-    std::vector<int> candies;
+        //This should loop until all test cases are done
+        for (int i{0}; i < test_cases; ++i) {
+            int boxes_no;
+            std::vector<int> candies;
 
-    std::cin >> boxes_no;
+            std::cin >> boxes_no;
+            if(boxes_no >= 1 && boxes_no <= 50) {//Only to satisfy constraints
 
-    //This should take all candies distribution input
-    for(int k{0}; k < boxes_no; ++k){
-        int temp_candies;
-        std::cin >> temp_candies;
-        candies.push_back(temp_candies);
-    }
-    sort(candies.begin(), candies.end());
+                //This should take all candies distribution input
+                for (int k{0}; k < boxes_no; ++k) {
+                    int temp_candies;
+                    std::cin >> temp_candies;
+                    candies.push_back(temp_candies);
+                }
+                sort(candies.begin(), candies.end());
 
-    int eaten_candies{0};
+                int eaten_candies{0};
 
-    for(int k {1}; k < boxes_no; ++k){
-        eaten_candies += candies[k] - candies[0];
-    }
+                for (int k{1}; k < boxes_no; ++k) {
+                    eaten_candies += candies[k] - candies[0];
+                }
 
-    std::cout << eaten_candies << "\n";
-
+                std::cout << eaten_candies << "\n";
+            }
+        }
     }
 }
