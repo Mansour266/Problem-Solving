7b1830ff42abc7a77edabff524553a6399960b1e
Mansour266 <mansour24@protonmail.com>
2022-06-02 16:19:15 +0200

    Solution accepted with 15ms and memory 0kb

---
+++ Codeforces/476A - A. Dreamoon and Stairs.cpp
@@ -0,0 +1,26 @@
+/*
+ * This is solution to problem 476A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/476/A
+ * Author: Mansour
+ * Last modified : 2/6/2022
+ */
+
+#include <iostream>
+
+int main() {
+    long int n, m, result;
+    std::cin >> n >> m;
+
+    if(n < m)
+        std::cout << "-1";
+    else{
+        if(n % 2 == 0)
+            result = n / 2;
+        else
+            result = (n / 2) + 1;
+        while (result % m != 0)
+            result++;
+        std::cout << result;
+    }
+
+}
