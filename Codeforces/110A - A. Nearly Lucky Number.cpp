e901e342950d72e1b7e2096893eb74ae1c630586
Mansour266 <mansour24@protonmail.com>
2022-07-19 02:31:19 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/110A - A. Nearly Lucky Number.cpp
@@ -0,0 +1,26 @@
+/*
+ * This is solution to problem 110A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/110/A
+ * Author: Mansour
+ * Last modified : 19/7/2022
+ */
+
+#include <bits/stdc++.h>
+
+int main(){
+    std::string no;
+    std::cin >> no;
+    int count{0};
+    for(int i {0}; i < no.length();++i){
+        if(no[i] != '4' && no[i] != '7'){
+            continue;
+        }
+        else {
+            count++;
+        }
+    }
+    if((count == 4 || count == 7) && no.length() > 1)
+        std::cout << "YES";
+    else
+        std::cout << "NO";
+}
