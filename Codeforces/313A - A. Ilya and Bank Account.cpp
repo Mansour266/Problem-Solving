6718f27155ac416176fe73ec6819d6f518671a18
Mansour266 <mansour24@Protonmail.com>
2022-08-25 21:53:12 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/313A - A. Ilya and Bank Account.cpp
@@ -0,0 +1,8 @@
+/*
+ * This is solution to problem 313A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/313/A
+ * Author: Mansour
+ * Last modified : 2/6/2022
+ */
+
+// Need to understand number theory first
