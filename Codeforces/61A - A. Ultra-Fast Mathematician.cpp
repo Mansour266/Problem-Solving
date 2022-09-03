692abf70df0632a8edf2c6f7365201ffc9470c62
Mansour266 <mansour24@Protonmail.com>
2022-09-03 19:41:11 +0200

    Solution accepted with 15ms and memory 0kb

---
+++ Codeforces/61A - A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,24 @@
+/*
+ * This is solution to problem 61A in Code force in C++
+ * Link: https://codeforces.com/problemset/problem/61/A
+ * Author: Mansour
+ * Last modified : 3/9/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    string first, second, result{""};
+    cin >> first >> second;
+    for(int i {0}; i < first.length(); ++i){
+        if(first[i] != second[i])
+            result += '1';
+        else
+            result += '0';
+    }
+    cout << result << '\n';
+}
