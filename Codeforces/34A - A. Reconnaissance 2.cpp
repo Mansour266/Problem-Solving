ca0cf9c53a36cf5cb25d1169f883a50a9adccd7e
Mansour266 <mansour24@Protonmail.com>
2022-09-26 21:23:43 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/34A - A. Reconnaissance 2.cpp
@@ -0,0 +1,32 @@
+/*
+ * This is solution to problem 155A in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/34/A
+ * Author: Mansour
+ * Last modified : 24/9/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0),cout.tie(0),ios::sync_with_stdio(0);
+    int n; cin >> n;
+    int arr[n];
+    for(int i{0}; i < n;++i){
+        cin >> arr[i];
+    }
+    int diff{abs(arr[0] - arr[n - 1])}, m{1001}, index1{1}, index2{n};
+    m = min(m, diff);
+    for(int i{0}; i < n - 1;++i){
+        diff = abs(arr[i] - arr[i + 1]);
+        if(m > diff){
+            m = diff;
+            index1 = i + 1;
+            index2 = i + 2;
+        }
+    }
+    cout << index1 << " " << index2 << '\n';
+
+    //Solved with a hint in the part of comparing(second loop)
+}
