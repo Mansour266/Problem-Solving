045d9accb42cec112cc22c131ffea8f3e2bf5c67
Mansour266 <mansour24@Protonmail.com>
2022-09-27 19:26:17 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/199A - A. Hexadecimal's theorem.cpp
@@ -0,0 +1,19 @@
+/*
+ * This is solution to problem 199A in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/199/A
+ * Author: Mansour
+ * Last modified : 26/9/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution_a_199() {
+    cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+    int n; cin >> n;
+    cout << 0 << " " << 0 << " " << n << '\n';
+
+    //100% Brain used.
+    //Hint from https://github.com/buglinjo/codeforces/blob/master/199A%20-%20Hexadecimal's%20theorem.cpp
+}
