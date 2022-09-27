c27f6cd25dff4443958ecac30b02ec0ce338fc91
Mansour266 <mansour24@Protonmail.com>
2022-09-27 20:25:12 +0200

    Solution accepted with 92ms and memory 0kb

---
+++ Codeforces/339B - Xenia and Ringroad.cpp
@@ -0,0 +1,23 @@
+/*
+ * This is solution to problem 339B in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/339/B
+ * Author: Mansour
+ * Last modified : 27/9/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+    int n,m,initial; cin >> n >> m >> initial;
+    int total{0};
+    while (--m){
+        int temp; cin >> temp;
+        if(initial > temp)
+            total++;
+        initial = temp;
+    }
+    cout << ((long long) n) * ((long long) total) + initial - 1 << '\n'; //This line is written after a hint
+}
