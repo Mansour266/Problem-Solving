1f39cea45f694f26c6abeab93e42124fb5f522c1
Mansour266 <mansour24@Protonmail.com>
2022-09-28 21:28:58 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/352A - A. Jeff and Digits.cpp
@@ -0,0 +1,44 @@
+/*
+ * This is solution to problem 352A in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/352/A
+ * Author: Mansour
+ * Last modified : 28/9/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+
+    int n; cin >> n;
+
+    int fives{0}, zeros{0};
+
+    for(int i{0};i < n;++i) {
+        int temp;
+        cin >> temp;
+        if (temp == 5) fives++;
+        else zeros++;
+    }
+    //This part was written with a hint
+    if((fives / 9) == 0){
+        if(zeros)
+            cout << "0\n";
+        else
+            cout << "-1\n";
+    }
+    else{
+        if(zeros){
+            for(int i{0}; i < 9 * (fives / 9);++i)
+                cout << "5";
+            for(int i{0}; i < zeros;++i)
+                cout << "0";
+            cout << '\n';
+        }
+        else
+            cout << "-1\n";
+
+    }
+}
