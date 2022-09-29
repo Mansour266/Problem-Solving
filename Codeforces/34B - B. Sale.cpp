8e2be31901055d49a9ac12422cd561316f521007
Mansour266 <mansour24@Protonmail.com>
2022-09-29 22:07:37 +0200

    Solution accepted with 30ms and memory 100kb

---
+++ Codeforces/34B - B. Sale.cpp
@@ -0,0 +1,32 @@
+    /*
+     * This is solution to problem 34A in Codeforces in C++
+     * Link: https://codeforces.com/problemset/problem/34/A
+     * Author: Mansour
+     * Last modified : 29/9/2022
+     */
+
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int main() {
+        cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+
+        int n,m, total{0}; cin >> n >> m;
+
+        multiset<int> st;
+
+        for(int i{0};i < n;++i){
+            int temp; cin >> temp;
+            st.insert(temp);
+        }
+
+        for(int i{0};i < m;++i){
+            auto it = st.begin();
+            if(*it > 0) break;
+            total += abs(*it);
+            st.erase(it);
+        }
+
+        cout << total <<'\n';
+    }
