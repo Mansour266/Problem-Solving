ca939c1c3f3292ae6e0d1ea2d544fdf45ce44d14
Mansour266 <mansour24@Protonmail.com>
2022-10-01 19:39:00 +0200

    Solution accepted with 30ms and memory 0kb

---
+++ Codeforces/221A - A. Little Elephant and Function.cpp
@@ -0,0 +1,23 @@
+/*
+ * This is solution to problem 221A in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/221/A
+ * Author: Mansour
+ * Last modified : 1/10/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution_a_221() {
+    cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+    int n; cin >> n;
+
+    cout << n;
+    for(int i{1};i < n;++i)
+        cout << ' ' << i;
+
+    cout << '\n';
+
+    //I can't believe that's the solution
+}
