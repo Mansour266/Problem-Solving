00c6bcc4409bf5f8e066796de032e7132980778d
Mansour266 <mansour24@Protonmail.com>
2022-10-03 20:02:16 +0200

    Solution accepted with 0ms and memory 0kb

---
+++ Codeforces/630A - A. Again Twenty Five!.cpp
@@ -0,0 +1,17 @@
+/*
+ * This is solution to problem 630A in Codeforces in C++
+ * Link: https://codeforces.com/problemset/problem/630/A
+ * Author: Mansour
+ * Last modified : 3/10/2022
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
+
+    string s; cin >> s;
+    cout << "25\n";
+}
